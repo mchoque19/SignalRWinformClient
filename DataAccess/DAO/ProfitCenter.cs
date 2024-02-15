dc823acb02171c2c2804cf6c28fc06b1cabb62bb
Joan Miquel Hernandez Juan <jmhernande@madisa.es>
2024-02-15 15:06:12 +0000

    Merged in MC-17-api-central-production (pull request #1)
    Se agrego la api de los centros de produccion
    
    * Se agrego la api de los centros de produccion
    
    * Se cambio el nombre al central de produccion a ProfitCenter

---
+++ DataAccess/DAO/ProfitCenter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DAO
+{
+	public class ProfitCenter
+	{
+		private int id;
+		private string name;	 
+		public int Id { get => id; set => id = value; }
+		public string Name { get => name; set => name = value; }
+ 
+	}
+}
---
+++ MonitorCocinaApi/DTO/ProfitCenterDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MonitorCocinaApi.DTO
+{
+    public class ProfitCenterDto
+	{
+        public int Id { get; set; }
+
+        public string Name {get; set;}
+    }
+}
